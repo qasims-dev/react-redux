f1f3e3bf5166db98c51040df6de032e049630c6c
qasims-dev <qasims.dev@gmail.com>
2019-08-19 16:34:43 +0300

    Added Redux functionality and Form+List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import List from "./js/components/List.jsx";
+import Form from "./js/components/Form.jsx";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>REACT REDUX APP</h1>
+      <h2>Articles</h2>
+      <hr />
+      <List />
+      <h2>Add a new Article</h2>
+      <Form />
     </div>
   );
 }
